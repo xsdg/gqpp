bd524214fb1aa6dc55dddac45110e35cf1a7c669
Colin Clark <cclark@carbon>
2020-01-20 12:59:22 +0000

    Addl Fix #742: Add appdata file
    https://github.com/BestImageViewer/geeqie/pull/742
    
    Install appdata file to /usr/share/metainfo
    Enable language translations
    Move screenshots to temporary location

--- po/regen_potfiles.sh
+++ po/regen_potfiles.sh
@@ -3,7 +3,7 @@
 #generate a patch to update POTFILES.in
 #Use like this: ./regen_potfiles.sh | patch -p0
 TMP=POTFILES.in.$$
-((find ../src/ -type f -name '*.c' ; find ../ -type f -name '*.desktop.in') | while read f; do
+((find ../src/ -type f -name '*.c' ; find ../ -type f -name '*.desktop.in' ; find ../ -type f -name '*.appdata.xml.in') | while read f; do
 	(echo $f | sed 's#^../##')
 done) | sort > $TMP
 diff -u POTFILES.in $TMP
