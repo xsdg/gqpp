8bc059f7d30225a5b0a7f1a99c73b0f4d0518ae5
Dmitry Marakasov <amdmi3@amdmi3.ru>
2023-10-04 21:13:38 +0300

    Use sigsetjmp instead of setjmp where appropriate

--- src/image-load-cr3.cc
+++ src/image-load-cr3.cc
@@ -353,7 +353,7 @@ static gboolean image_loader_cr3_load (gpointer loader, const guchar *buf, gsize
         jerr.error = error;
 
 
-	if (setjmp(jerr.setjmp_buffer))
+	if (sigsetjmp(jerr.setjmp_buffer, 0))
 		{
 		/* If we get here, the JPEG code has signaled an error.
 		 * We need to clean up the JPEG object, close the input file, and return.
