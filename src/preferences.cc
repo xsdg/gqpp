xsdg/gqpp

commit 19c110d5de9787ca7a67f1af85d19bbdf5a5d513

GTK4: gtk_dialog_run()
GTK4 migration

- gtk_dialog_run() No longer supported - re-code
- Remaining instances will be removed when GApplication is implmented
- File dialogs are now modal

src/preferences.cc
@@ -63,7 +63,7 @@
 #define EDITOR_NAME_MAX_LENGTH 32
 #define EDITOR_COMMAND_MAX_LENGTH 1024
 
-static void image_overlay_set_text_colours();
+static void image_overlay_set_text_colors();
 
 GtkWidget *keyword_text;
 static void config_tab_keywords_save();
@@ -382,6 +382,7 @@ static void config_window_apply()
 	options->image_overlay.background_green = c_options->image_overlay.background_green;
 	options->image_overlay.background_blue = c_options->image_overlay.background_blue;
 	options->image_overlay.background_alpha = c_options->image_overlay.background_alpha;
+	options->image_overlay.font = c_options->image_overlay.font;
 	options->update_on_time_change = c_options->update_on_time_change;
 
 	options->duplicates_similarity_threshold = c_options->duplicates_similarity_threshold;
@@ -1445,74 +1446,145 @@ static void image_overlay_help_cb(GtkWidget *, gpointer)
 	help_window_show("GuideOptionsOSD.html");
 }
 
-static void image_overlay_set_font_cb(GtkWidget *widget, gpointer)
+static void font_activated_cb(GtkFontChooser *widget, gchar *fontname, gpointer)
 {
-	GtkWidget *dialog;
-	char *font;
-	PangoFontDescription *font_desc;
+	g_free(c_options->image_overlay.font);
+	c_options->image_overlay.font = g_strdup(fontname);
+	g_free(fontname);
 
-	dialog = gtk_font_chooser_dialog_new("Image Overlay Font", GTK_WINDOW(gtk_widget_get_toplevel(widget)));
-	gtk_font_chooser_set_font(GTK_FONT_CHOOSER(dialog), options->image_overlay.font);
+	gtk_widget_destroy(GTK_WIDGET(widget));
+}
 
-	if (gtk_dialog_run(GTK_DIALOG(dialog)) != GTK_RESPONSE_CANCEL)
+static void font_response_cb(GtkDialog *dialog, gint response_id, gpointer)
+{
+	gchar *font;
+
+	g_free(c_options->image_overlay.font);
+	c_options->image_overlay.font = g_strdup(options->image_overlay.font);
+
+	if (response_id == GTK_RESPONSE_OK)
 		{
-		font_desc = gtk_font_chooser_get_font_desc(GTK_FONT_CHOOSER(dialog));
-		font = pango_font_description_to_string(font_desc);
+		font = gtk_font_chooser_get_font(GTK_FONT_CHOOSER(dialog));
 		g_free(c_options->image_overlay.font);
 		c_options->image_overlay.font = g_strdup(font);
 		g_free(font);
 		}
 
-	gtk_widget_destroy(dialog);
+	gtk_widget_destroy(GTK_WIDGET(dialog));
 }
 
-static void image_overlay_set_text_colour_cb(GtkWidget *widget, gpointer)
+static void image_overlay_set_font_cb(GtkWidget *widget, gpointer)
 {
 	GtkWidget *dialog;
-	GdkRGBA colour;
 
-	dialog = gtk_color_chooser_dialog_new("Image Overlay Text Colour", GTK_WINDOW(gtk_widget_get_toplevel(widget)));
-	colour.red = options->image_overlay.text_red;
-	colour.green = options->image_overlay.text_green;
-	colour.blue = options->image_overlay.text_blue;
-	colour.alpha = options->image_overlay.text_alpha;
-	gtk_color_chooser_set_rgba(GTK_COLOR_CHOOSER(dialog), &colour);
-	gtk_color_chooser_set_use_alpha(GTK_COLOR_CHOOSER(dialog), TRUE);
+	dialog = gtk_font_chooser_dialog_new("Image Overlay Font", GTK_WINDOW(gtk_widget_get_toplevel(widget)));
+	gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
+	gtk_font_chooser_set_font(GTK_FONT_CHOOSER(dialog), options->image_overlay.font);
 
-	if (gtk_dialog_run(GTK_DIALOG(dialog)) != GTK_RESPONSE_CANCEL)
-		{
-		gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(dialog), &colour);
-		c_options->image_overlay.text_red = colour.red*255;
-		c_options->image_overlay.text_green = colour.green*255;
-		c_options->image_overlay.text_blue = colour.blue*255;
-		c_options->image_overlay.text_alpha = colour.alpha*255;
-		}
-	gtk_widget_destroy(dialog);
+	g_signal_connect(dialog, "font-activated", G_CALLBACK(font_activated_cb), nullptr);
+	g_signal_connect(dialog, "response", G_CALLBACK(font_response_cb), nullptr);
+
+	gtk_widget_show(dialog);
 }
 
+static void text_color_activated_cb(GtkColorChooser *chooser, GdkRGBA *color, gpointer)
+{
+	c_options->image_overlay.text_red = color->red * 255;
+	c_options->image_overlay.text_green = color->green * 255;
+	c_options->image_overlay.text_blue = color->blue * 255;
+	c_options->image_overlay.text_alpha = color->alpha * 255;
 
-static void image_overlay_set_background_colour_cb(GtkWidget *widget, gpointer)
+	gtk_widget_destroy(GTK_WIDGET(chooser));
+}
+
+static void text_color_response_cb(GtkDialog *dialog, gint response_id, gpointer)
+{
+	GdkRGBA color;
+
+	c_options->image_overlay.text_red = options->image_overlay.text_red;
+	c_options->image_overlay.text_green = options->image_overlay.text_green;
+	c_options->image_overlay.text_blue = options->image_overlay.text_blue;
+	c_options->image_overlay.text_alpha = options->image_overlay.text_alpha;
+
+	if (response_id == GTK_RESPONSE_OK)
+		{
+		gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(dialog), &color);
+		c_options->image_overlay.text_red = color.red * 255;
+		c_options->image_overlay.text_green = color.green * 255;
+		c_options->image_overlay.text_blue = color.blue * 255;
+		c_options->image_overlay.text_alpha = color.alpha * 255;
+		}
+
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+}
+
+static void image_overlay_set_text_color_cb(GtkWidget *widget, gpointer)
 {
 	GtkWidget *dialog;
-	GdkRGBA colour;
+	GdkRGBA color;
 
-	dialog = gtk_color_chooser_dialog_new("Image Overlay Background Colour", GTK_WINDOW(gtk_widget_get_toplevel(widget)));
-	colour.red = options->image_overlay.background_red;
-	colour.green = options->image_overlay.background_green;
-	colour.blue = options->image_overlay.background_blue;
-	colour.alpha = options->image_overlay.background_alpha;
-	gtk_color_chooser_set_rgba(GTK_COLOR_CHOOSER(dialog), &colour);
+	dialog = gtk_color_chooser_dialog_new(_("Image Overlay Text Color"), GTK_WINDOW(gtk_widget_get_toplevel(widget)));
+	color.red = options->image_overlay.text_red;
+	color.green = options->image_overlay.text_green;
+	color.blue = options->image_overlay.text_blue;
+	color.alpha = options->image_overlay.text_alpha;
+	gtk_color_chooser_set_rgba(GTK_COLOR_CHOOSER(dialog), &color);
 	gtk_color_chooser_set_use_alpha(GTK_COLOR_CHOOSER(dialog), TRUE);
 
-	if (gtk_dialog_run(GTK_DIALOG(dialog)) != GTK_RESPONSE_CANCEL)
+	g_signal_connect(dialog, "color-activated", G_CALLBACK(text_color_activated_cb), nullptr);
+	g_signal_connect(dialog, "response", G_CALLBACK(text_color_response_cb), nullptr);
+
+	gtk_widget_show(dialog);
+}
+
+static void bg_color_activated_cb(GtkColorChooser *chooser, GdkRGBA *color, gpointer)
+{
+	c_options->image_overlay.background_red = color->red * 255;
+	c_options->image_overlay.background_green = color->green * 255;
+	c_options->image_overlay.background_blue = color->blue * 255;
+	c_options->image_overlay.background_alpha = color->alpha * 255;
+
+	gtk_widget_destroy(GTK_WIDGET(chooser));
+}
+
+static void bg_color_response_cb(GtkDialog *dialog, gint response_id, gpointer)
+{
+	GdkRGBA color;
+
+	c_options->image_overlay.background_red = options->image_overlay.background_red;
+	c_options->image_overlay.background_green = options->image_overlay.background_green;
+	c_options->image_overlay.background_blue = options->image_overlay.background_blue;
+	c_options->image_overlay.background_alpha = options->image_overlay.background_alpha;
+
+	if (response_id == GTK_RESPONSE_OK)
 		{
-		gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(dialog), &colour);
-		c_options->image_overlay.background_red = colour.red*255;
-		c_options->image_overlay.background_green = colour.green*255;
-		c_options->image_overlay.background_blue = colour.blue*255;
-		c_options->image_overlay.background_alpha = colour.alpha*255;
+		gtk_color_chooser_get_rgba(GTK_COLOR_CHOOSER(dialog), &color);
+		c_options->image_overlay.background_red = color.red * 255;
+		c_options->image_overlay.background_green = color.green * 255;
+		c_options->image_overlay.background_blue = color.blue * 255;
+		c_options->image_overlay.background_alpha = color.alpha * 255;
 		}
-	gtk_widget_destroy(dialog);
+
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+}
+
+static void image_overlay_set_background_color_cb(GtkWidget *widget, gpointer)
+{
+	GtkWidget *dialog;
+	GdkRGBA color;
+
+	dialog = gtk_color_chooser_dialog_new(_("Image Overlay Background Color"), GTK_WINDOW(gtk_widget_get_toplevel(widget)));
+	color.red = options->image_overlay.background_red;
+	color.green = options->image_overlay.background_green;
+	color.blue = options->image_overlay.background_blue;
+	color.alpha = options->image_overlay.background_alpha;
+	gtk_color_chooser_set_rgba(GTK_COLOR_CHOOSER(dialog), &color);
+	gtk_color_chooser_set_use_alpha(GTK_COLOR_CHOOSER(dialog), TRUE);
+
+	g_signal_connect(dialog, "color-activated", G_CALLBACK(bg_color_activated_cb), nullptr);
+	g_signal_connect(dialog, "response", G_CALLBACK(bg_color_response_cb), nullptr);
+
+	gtk_widget_show(dialog);
 }
 
 static void accel_store_populate()
@@ -2427,16 +2499,14 @@ static void config_tab_osd(GtkWidget *notebook)
 	gq_gtk_box_pack_start(GTK_BOX(hbox), button, FALSE, FALSE, 0);
 	gtk_widget_show(button);
 
-	button = pref_button_new(nullptr, GQ_ICON_SELECT_COLOR, _("Text"),
-				 G_CALLBACK(image_overlay_set_text_colour_cb), nullptr);
+	button = pref_button_new(nullptr, GQ_ICON_SELECT_COLOR, _("Text"), G_CALLBACK(image_overlay_set_text_color_cb), nullptr);
 	gq_gtk_box_pack_start(GTK_BOX(hbox), button, FALSE, FALSE, 0);
 	gtk_widget_show(button);
 
-	button = pref_button_new(nullptr, GQ_ICON_SELECT_COLOR, _("Background"),
-				 G_CALLBACK(image_overlay_set_background_colour_cb), nullptr);
+	button = pref_button_new(nullptr, GQ_ICON_SELECT_COLOR, _("Background"), G_CALLBACK(image_overlay_set_background_color_cb), nullptr);
 	gq_gtk_box_pack_start(GTK_BOX(hbox), button, FALSE, FALSE, 0);
 	gtk_widget_show(button);
-	image_overlay_set_text_colours();
+	image_overlay_set_text_colors();
 
 	button = pref_button_new(nullptr, nullptr, _("Defaults"),
 				 G_CALLBACK(image_overlay_default_template_cb), image_overlay_template_view);
@@ -4163,7 +4233,7 @@ void show_about_window(LayoutWindow *lw)
 	g_object_unref(in_stream_translators);
 }
 
-static void image_overlay_set_text_colours()
+static void image_overlay_set_text_colors()
 {
 	c_options->image_overlay.text_red = options->image_overlay.text_red;
 	c_options->image_overlay.text_green = options->image_overlay.text_green;

src/print.cc
@@ -120,9 +120,34 @@ static gboolean print_job_render_image(PrintWindow *pw)
 	return TRUE;
 }
 
+static void font_activated_cb(GtkFontChooser *widget, gchar *fontname, gpointer option)
+{
+	option = g_strdup(fontname);
+
+	g_free(fontname);
+
+	gtk_widget_destroy(GTK_WIDGET(widget));
+}
+
+static void font_response_cb(GtkDialog *dialog, int response_id, gpointer option)
+{
+	gchar *font;
+
+	if (response_id == GTK_RESPONSE_OK)
+		{
+		font = gtk_font_chooser_get_font(GTK_FONT_CHOOSER(dialog));
+		g_free(option);
+		option = g_strdup(font);
+		g_free(font);
+		}
+
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+}
+
 static void print_set_font_cb(GtkWidget *widget, gpointer data)
 {
 	gpointer option;
+	GtkWidget *dialog;
 
 	if (g_strcmp0(static_cast<const gchar *>(data), "Image text font") == 0)
 		{
@@ -133,23 +158,14 @@ static void print_set_font_cb(GtkWidget *widget, gpointer data)
 		option = options->printer.page_font;
 		}
 
-	GtkWidget *dialog;
-	char *font;
-	PangoFontDescription *font_desc;
-
 	dialog = gtk_font_chooser_dialog_new(static_cast<const gchar *>(data), GTK_WINDOW(gtk_widget_get_toplevel(widget)));
+	gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
 	gtk_font_chooser_set_font(GTK_FONT_CHOOSER(dialog), static_cast<const gchar *>(option));
 
-	if (gtk_dialog_run(GTK_DIALOG(dialog)) != GTK_RESPONSE_CANCEL)
-		{
-		font_desc = gtk_font_chooser_get_font_desc(GTK_FONT_CHOOSER(dialog));
-		font = pango_font_description_to_string(font_desc);
-		g_free(option);
-		option = g_strdup(font);
-		g_free(font);
-		}
+	g_signal_connect(dialog, "font-activated", G_CALLBACK(font_activated_cb), option);
+	g_signal_connect(dialog, "response", G_CALLBACK(font_response_cb), option);
 
-	gtk_widget_destroy(dialog);
+	gtk_widget_show(dialog);
 }
 
 static gint set_toggle(GSList *list, TextPosition pos)

src/ui-pathsel.cc
@@ -701,58 +701,51 @@ static gboolean dest_keypress_cb(GtkWidget *view, GdkEventKey *event, gpointer d
 	return FALSE;
 }
 
+static void file_util_create_dir_cb(gboolean success, const gchar *new_path, gpointer data)
+{
+	auto dd = static_cast<Dest_Data *>(data);
+	const gchar *text;
+	GtkListStore *store;
+	GtkTreeIter iter;
+
+	if (!success)
+		{
+		return;
+		}
+
+	if (new_path == nullptr)
+		{
+		return;
+		}
+
+	store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(dd->d_view)));
+
+	text = filename_from_path(new_path);
+
+	gtk_list_store_append(store, &iter);
+	gtk_list_store_set(store, &iter, 0, text, 1, new_path, -1);
+
+	if (dd->right_click_path)
+		{
+		gtk_tree_path_free(dd->right_click_path);
+		}
+	dd->right_click_path = gtk_tree_model_get_path(GTK_TREE_MODEL(store), &iter);
+
+	gtk_entry_set_text(GTK_ENTRY(dd->entry), new_path);
+
+	gtk_widget_grab_focus(GTK_WIDGET(dd->entry));
+}
+
 static void dest_new_dir_cb(GtkWidget *widget, gpointer data)
 {
 	auto dd = static_cast<Dest_Data *>(data);
-	gchar *path;
-	GtkWidget *dialog_window;
 
 /**
  * @FIXME on exit from the "new folder" modal dialog, focus returns to the main Geeqie
  * window rather than the file dialog window. gtk_window_present() does not seem to
  * function unless the window was previously minimized.
  */
-	dialog_window = gtk_widget_get_toplevel(widget);
-	gtk_window_iconify(GTK_WINDOW(dialog_window));
-	path = new_folder(GTK_WINDOW(dialog_window), dd->path);
-	gtk_window_present(GTK_WINDOW(dialog_window));
-
-	if (path == nullptr)
-		{
-		return;
-		}
-
-	if (!mkdir_utf8(path, 0755))
-		{
-		/* failed */
-		gchar *text;
-
-		text = g_strdup_printf(_("Unable to create folder:\n%s"), filename_from_path(path));
-		warning_dialog(_("Error creating folder"), text, GQ_ICON_DIALOG_ERROR, dd->entry);
-		g_free(text);
-		}
-	else
-		{
-		GtkTreeIter iter;
-		GtkListStore *store;
-		const gchar *text;
-
-		store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(dd->d_view)));
-
-		text = filename_from_path(path);
-
-		gtk_list_store_append(store, &iter);
-		gtk_list_store_set(store, &iter, 0, text, 1, path, -1);
-
-		if (dd->right_click_path) gtk_tree_path_free(dd->right_click_path);
-		dd->right_click_path = gtk_tree_model_get_path(GTK_TREE_MODEL(store), &iter);
-
-		gtk_entry_set_text(GTK_ENTRY(dd->entry), path);
-		}
-
-	gtk_widget_grab_focus(GTK_WIDGET(dd->entry));
-
-	g_free(path);
+	file_util_create_dir(gtk_entry_get_text(GTK_ENTRY(dd->entry)), widget, file_util_create_dir_cb, data);
 }
 
 /*

src/utilops.cc
@@ -224,6 +224,8 @@ FileDialog *file_util_file_dlg(const gchar *title,
 		gtk_window_set_position(GTK_WINDOW(GENERIC_DIALOG(fdlg)->dialog), GTK_WIN_POS_MOUSE);
 		}
 
+	gtk_window_set_modal(GTK_WINDOW(fdlg->gd.dialog), TRUE);
+
 	return fdlg;
 }
 
@@ -357,6 +359,7 @@ struct UtilityData {
 
 	gchar *external_command;
 	gpointer resume_data;
+	gboolean show_rename_button;
 
 	FileUtilDoneFunc done_func;
 	void (*details_func)(UtilityData *ud, FileData *fd);
@@ -444,6 +447,15 @@ static UtilityData *file_util_data_new(UtilityType type)
 
 	ud->type = type;
 	ud->phase = UTILITY_PHASE_START;
+
+	if (type == UTILITY_TYPE_CREATE_FOLDER)
+		{
+		ud->show_rename_button = FALSE;
+		}
+	else
+		{
+		ud->show_rename_button = TRUE;
+		}
 
 	return ud;
 }
@@ -1633,14 +1645,21 @@ static void file_util_dialog_init_dest_folder(UtilityData *ud)
 
 	pref_spacer(GENERIC_DIALOG(fdlg)->vbox, 0);
 
-	if (options->with_rename)
+	if (ud->show_rename_button == TRUE)
 		{
-		file_dialog_add_button(fdlg, icon_name, ud->messages.title, file_util_fdlg_ok_cb, TRUE);
-		file_dialog_add_button(fdlg, GQ_ICON_EDIT, _("With Rename"), file_util_fdlg_rename_cb, TRUE);
+		if (options->with_rename)
+			{
+			file_dialog_add_button(fdlg, icon_name, ud->messages.title, file_util_fdlg_ok_cb, TRUE);
+			file_dialog_add_button(fdlg, GQ_ICON_EDIT, _("With Rename"), file_util_fdlg_rename_cb, TRUE);
+			}
+		else
+			{
+			file_dialog_add_button(fdlg, GQ_ICON_EDIT, _("With Rename"), file_util_fdlg_rename_cb, TRUE);
+			file_dialog_add_button(fdlg, icon_name, ud->messages.title, file_util_fdlg_ok_cb, TRUE);
+			}
 		}
 	else
 		{
-		file_dialog_add_button(fdlg, GQ_ICON_EDIT, _("With Rename"), file_util_fdlg_rename_cb, TRUE);
 		file_dialog_add_button(fdlg, icon_name, ud->messages.title, file_util_fdlg_ok_cb, TRUE);
 		}
 
@@ -2902,7 +2921,7 @@ static void file_util_rename_dir_full(FileData *fd, const gchar *new_path, GtkWi
 	file_util_dialog_run(ud);
 }
 
-static void file_util_create_dir_full(FileData *fd, const gchar *dest_path, GtkWidget *parent, UtilityPhase phase, FileUtilDoneFunc done_func, gpointer done_data)
+static void file_util_create_dir_full(const gchar *path, const gchar *dest_path, GtkWidget *parent, UtilityPhase phase, FileUtilDoneFunc done_func, gpointer done_data)
 {
 	UtilityData *ud;
 
@@ -2923,17 +2942,9 @@ static void file_util_create_dir_full(FileData *fd, const gchar *dest_path, GtkW
 		}
 	else
 		{
-		gchar *buf = new_folder(GTK_WINDOW(parent), fd->path);
-		if (!buf)
-			{
-			ud->phase = UTILITY_PHASE_CANCEL;
-			ud->dir_fd = nullptr;
-			}
-		else
-			{
-			ud->dest_path = buf;
-			ud->dir_fd = file_data_new_dir(ud->dest_path);
-			}
+		gchar *buf = g_build_filename(path, _("New folder"), nullptr);
+		ud->dest_path = unique_filename(buf, nullptr, " ", FALSE);
+		g_free(buf);
 		}
 
 	ud->done_func = done_func;
@@ -2947,7 +2958,6 @@ static void file_util_create_dir_full(FileData *fd, const gchar *dest_path, GtkW
 
 	file_util_dialog_run(ud);
 }
-
 
 static gboolean file_util_write_metadata_first_after_done(gpointer data)
 {
@@ -3114,9 +3124,9 @@ void file_util_delete_dir(FileData *fd, GtkWidget *parent)
 	file_util_delete_dir_full(fd, parent, UTILITY_PHASE_START);
 }
 
-void file_util_create_dir(FileData *dir_fd, GtkWidget *parent, FileUtilDoneFunc done_func, gpointer done_data)
+void file_util_create_dir(const gchar *path, GtkWidget *parent, FileUtilDoneFunc done_func, gpointer done_data)
 {
-	file_util_create_dir_full(dir_fd, nullptr, parent, UTILITY_PHASE_ENTERING, done_func, done_data);
+	file_util_create_dir_full(path, nullptr, parent, UTILITY_PHASE_START, done_func, done_data);
 }
 
 void file_util_rename_dir(FileData *source_fd, const gchar *new_path, GtkWidget *parent, FileUtilDoneFunc done_func, gpointer done_data)
@@ -3289,119 +3299,4 @@ void file_util_copy_path_list_to_clipboard(GList *fd_list, gboolean quoted)
 	filelist_free(fd_list);
 }
 
-static void new_folder_entry_activate_cb(GtkWidget *, gpointer data)
-{
-	auto dialog = static_cast<GtkDialog *>(data);
-
-	gtk_dialog_response(dialog, GTK_RESPONSE_ACCEPT);
-}
-
-gchar *new_folder(GtkWindow *window , gchar *path)
-{
-	GtkWidget *hbox;
-	GtkWidget *vbox;
-	GtkWidget *label;
-	gchar *buf;
-	gchar *folder_name;
-	gchar *folder_path;
-	GtkWidget *image;
-	GtkWidget *folder_name_entry;
-	GtkWidget *dialog;
-	GtkWidget *dialog_warning;
-	GtkWidget *content_area;
-	gboolean ok_or_cancel = FALSE;
-	gint result;
-	gchar *new_folder_name;
-	gchar *new_folder_path_full = nullptr;
-	gchar *window_title;
-	PangoLayout *layout;
-	gint width, height;
-
-	buf = g_build_filename(path, _("New folder"), NULL);
-	folder_path = unique_filename(buf, nullptr, " ", FALSE);
-	folder_name = g_path_get_basename(folder_path);
-	window_title = g_strconcat(_("Create Folder - "), GQ_APPNAME, NULL);
-
-	dialog = gtk_dialog_new_with_buttons(window_title,
-										GTK_WINDOW(window),
-										GTK_DIALOG_MODAL,
-										GTK_STOCK_CANCEL,
-										GTK_RESPONSE_REJECT,
-										GTK_STOCK_OK,
-										GTK_RESPONSE_ACCEPT,
-										NULL);
-
-	layout = gtk_widget_create_pango_layout(GTK_WIDGET(dialog), window_title);
-	pango_layout_get_pixel_size(layout, &width, &height);
-	gtk_window_set_default_size(GTK_WINDOW(dialog), width * 2, -1);
-
-	content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
-	vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, PREF_PAD_GAP);
-	gq_gtk_box_pack_start(GTK_BOX(content_area), vbox, FALSE, FALSE, 0);
-
-	hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, PREF_PAD_GAP);
-	gtk_container_set_border_width(GTK_CONTAINER(hbox), PREF_PAD_GAP);
-	gq_gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
-	image = gtk_image_new_from_icon_name(GQ_ICON_DIALOG_QUESTION, GTK_ICON_SIZE_DIALOG);
-	gq_gtk_box_pack_start(GTK_BOX(hbox), image, FALSE, FALSE, 0);
-	label = gtk_label_new(_("Create new folder"));
-	gq_gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 0);
-
-	folder_name_entry = gtk_entry_new();
-	gtk_entry_set_text(GTK_ENTRY(folder_name_entry), folder_name);
-	gq_gtk_box_pack_start(GTK_BOX(vbox), folder_name_entry, FALSE, FALSE, PREF_PAD_SPACE);
-	g_signal_connect(G_OBJECT(folder_name_entry), "activate", G_CALLBACK(new_folder_entry_activate_cb), dialog);
-
-	gq_gtk_widget_show_all(dialog);
-
-	while (ok_or_cancel == FALSE)
-		{
-		result = gtk_dialog_run(GTK_DIALOG(dialog));
-
-		switch (result)
-			{
-			case GTK_RESPONSE_ACCEPT:
-				new_folder_name = g_strdup(gtk_entry_get_text(GTK_ENTRY(folder_name_entry)));
-				new_folder_path_full = g_build_filename(path, new_folder_name, NULL);
-				if (isname(new_folder_path_full))
-					{
-					dialog_warning = gtk_message_dialog_new(GTK_WINDOW(window),
-											GTK_DIALOG_MODAL,
-											GTK_MESSAGE_WARNING,
-											GTK_BUTTONS_OK,
-											_("Cannot create folder:"));
-					gtk_message_dialog_format_secondary_text(GTK_MESSAGE_DIALOG(dialog_warning), "%s", new_folder_name);
-					gtk_window_set_title(GTK_WINDOW(dialog_warning), GQ_APPNAME);
-
-					gtk_dialog_run(GTK_DIALOG(dialog_warning));
-
-					gtk_widget_destroy(dialog_warning);
-					g_free(new_folder_path_full);
-					new_folder_path_full = nullptr;
-					g_free(new_folder_name);
-					ok_or_cancel = FALSE;
-					}
-				else
-					{
-					ok_or_cancel = TRUE;
-					}
-				break;
-			case GTK_RESPONSE_REJECT:
-				ok_or_cancel = TRUE;
-				break;
-			default:
-				ok_or_cancel = TRUE;
-				break;
-		  }
-		}
-
-	g_free(buf);
-	g_free(folder_path);
-	g_free(folder_name);
-	g_object_unref(layout);
-	g_free(window_title);
-	gtk_widget_destroy(dialog);
-
-	return new_folder_path_full;
-}
 /* vim: set shiftwidth=8 softtabstop=0 cindent cinoptions={1s: */

src/utilops.h
@@ -51,7 +51,7 @@ void file_util_copy(FileData *source_fd, GList *source_list, const gchar *dest_p
 void file_util_rename(FileData *source_fd, GList *source_list, GtkWidget *parent);
 void file_util_write_metadata(FileData *source_fd, GList *source_list, GtkWidget *parent, gboolean force_dialog, FileUtilDoneFunc done_func, gpointer done_data);
 
-void file_util_create_dir(FileData *dir_fd, GtkWidget *parent, FileUtilDoneFunc done_func, gpointer done_data);
+void file_util_create_dir(const gchar *path, GtkWidget *parent, FileUtilDoneFunc done_func, gpointer done_data);
 
 void file_util_rename_dir(FileData *source_fd, const gchar *new_path, GtkWidget *parent, FileUtilDoneFunc done_func, gpointer done_data);
 

src/view-dir.cc
@@ -702,7 +702,7 @@ static void vd_pop_menu_new_cb(GtkWidget *, gpointer data)
 			break;
 		}
 
-	file_util_create_dir(dir_fd, vd->layout->window, vd_pop_menu_new_folder_cb, vd);
+	file_util_create_dir(dir_fd->path, vd->layout->window, vd_pop_menu_new_folder_cb, vd);
 }
 
 static void vd_pop_menu_rename_cb(GtkWidget *, gpointer data)
@@ -874,7 +874,7 @@ GtkWidget *vd_pop_menu(ViewDir *vd, FileData *fd)
 
 void vd_new_folder(ViewDir *vd, FileData *dir_fd)
 {
-	file_util_create_dir(dir_fd, vd->layout->window, vd_pop_menu_new_folder_cb, vd);
+	file_util_create_dir(dir_fd->path, vd->layout->window, vd_pop_menu_new_folder_cb, vd);
 }
 
 /*

commit 9554963fcddbab06ca2d03b092680629d4600123

Fix #1359: Geequie randomly forgets option settings
https://github.com/BestImageViewer/geeqie/issues/1359

Option image_overlay.font had uninitialzed data written to it when the
preferences dialog was closed.

On restart the options parser detected an error which caused the entire
geeqierc.xml file to be ignored, and the defaults were used instead.

Bug introduced in 19c110d5

src/preferences.cc
@@ -426,7 +426,6 @@ static void config_window_apply()
 	options->image_overlay.background_green = c_options->image_overlay.background_green;
 	options->image_overlay.background_blue = c_options->image_overlay.background_blue;
 	options->image_overlay.background_alpha = c_options->image_overlay.background_alpha;
-	options->image_overlay.font = c_options->image_overlay.font;
 	options->update_on_time_change = c_options->update_on_time_change;
 
 	options->duplicates_similarity_threshold = c_options->duplicates_similarity_threshold;