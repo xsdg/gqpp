xsdg/gqpp

commit 138b03ff6a826f680532ce43389f82b1ebb3d9e9

Include check of bash completions file
- In test-ancillary-files.sh include a check to ensure all options are
in the bash completions file
- Fix identified errors
- Rename crop actions so that they are grouped together

src/layout-util.cc
@@ -2900,11 +2900,11 @@ static GtkRadioActionEntry menu_stereo_mode_entries[] = {
 };
 
 static GtkRadioActionEntry menu_draw_rectangle_aspect_ratios[] = {
-  { "None",        nullptr, N_("None"), nullptr, N_("None"), RECTANGLE_DRAW_ASPECT_RATIO_NONE },
-  { "OneOne",      nullptr, N_("1:1"),  nullptr, N_("1:1"),  RECTANGLE_DRAW_ASPECT_RATIO_ONE_ONE },
-  { "FourThree",   nullptr, N_("4:3"),  nullptr, N_("4:3"),  RECTANGLE_DRAW_ASPECT_RATIO_FOUR_THREE },
-  { "ThreeTwo",    nullptr, N_("3:2"),  nullptr, N_("3:2"),  RECTANGLE_DRAW_ASPECT_RATIO_THREE_TWO },
-  { "SixteenNine", nullptr, N_("16:9"), nullptr, N_("16:9"), RECTANGLE_DRAW_ASPECT_RATIO_SIXTEEN_NINE }
+  { "CropNone",        nullptr, N_("None"), nullptr, N_("None"), RECTANGLE_DRAW_ASPECT_RATIO_NONE },
+  { "CropOneOne",      nullptr, N_("1:1"),  nullptr, N_("1:1"),  RECTANGLE_DRAW_ASPECT_RATIO_ONE_ONE },
+  { "CropFourThree",   nullptr, N_("4:3"),  nullptr, N_("4:3"),  RECTANGLE_DRAW_ASPECT_RATIO_FOUR_THREE },
+  { "CropThreeTwo",    nullptr, N_("3:2"),  nullptr, N_("3:2"),  RECTANGLE_DRAW_ASPECT_RATIO_THREE_TWO },
+  { "CropSixteenNine", nullptr, N_("16:9"), nullptr, N_("16:9"), RECTANGLE_DRAW_ASPECT_RATIO_SIXTEEN_NINE }
 };
 #undef CB
 

auto-complete/geeqie
@@ -2,11 +2,11 @@
 
 file_types='@(3fr|ani|arw|avif|bmp|cr2|cr3|crw|cur|dds|djvu|dng|erf|gif|heic|heif|ico|jp2|jpe[g]||jpegxl|jps|kdc|mef|mos|mpo|mrw|nef|orf|pbm|pdf|pgm|pnm|ppm|pef|png|psd|qif|qtif|raf|raw|rw2|scr|sr2|srf|svg[z]|tga|targa|tif[f]|webp|xbm|xpm|gqv|xml|xmp)'
 
-actions='About AddMark0 AddMark1 AddMark2 AddMark3 AddMark4 AddMark5 AddMark6 AddMark7 AddMark8 AddMark9 AlterNone Animate Back ClearMarks CloseWindow ColorProfile0 ColorProfile1 ColorProfile2 ColorProfile3 ColorProfile4 ColorProfile5 ConnectZoom100 ConnectZoom200 ConnectZoom25 ConnectZoom300 ConnectZoom33 ConnectZoom400 ConnectZoom50 ConnectZoomFillHor ConnectZoomFillVert ConnectZoomFit ConnectZoomIn ConnectZoomOut Copy CopyPath CopyPathUnquoted CutPath Delete DeleteWindow DrawRectangle Escape ExifRotate ExifWin FilterMark0 FilterMark1 FilterMark2 FilterMark3 FilterMark4 FilterMark5 FilterMark6 FilterMark7 FilterMark8 FilterMark9 FindDupes FirstImage FirstPage Flip FloatTools FolderTree Forward FullScreen Grayscale HelpChangeLog HelpContents HelpKbd HelpNotes HelpSearch HelpShortcuts HideBars HideSelectableToolbars HideTools HistogramChanB HistogramChanCycle HistogramChanG HistogramChanR HistogramChanRGB HistogramChanV HistogramModeCycle HistogramModeLin HistogramModeLog Home IgnoreAlpha ImageBack ImageForward ImageHistogram ImageOverlay ImageOverlayCycle IntMark0 IntMark1 IntMark2 IntMark3 IntMark4 IntMark5 IntMark6 IntMark7 IntMark8 IntMark9 KeywordAutocomplete LastImage LastPage LayoutConfig LogWindow Maintenance Mark0 Mark1 Mark2 Mark3 Mark4 Mark5 Mark6 Mark7 Mark8 Mark9 Mirror Move NewCollection NewFolder NewWindow NewWindowDefault NewWindowFromCurrent NextImage NextPage OpenArchive OpenCollection OpenRecent OpenWith OverUnderExposed PanView PermanentDelete Plugins Preferences PrevImage PrevPage Print Quit Rating0 Rating1 Rating2 Rating3 Rating4 Rating5 RatingM1 RectangularSelection Refresh Rename RenameWindow ResetMark0 ResetMark1 ResetMark2 ResetMark3 ResetMark4 ResetMark5 ResetMark6 ResetMark7 ResetMark8 ResetMark9 Rotate180 RotateCCW RotateCW SBar SBarSort SaveMetadata Search SearchAndRunCommand SelectAll SelectInvert SelectMark0 SelectMark1 SelectMark2 SelectMark3 SelectMark4 SelectMark5 SelectMark6 SelectMark7 SelectMark8 SelectMark9 SelectNone SetMark0 SetMark1 SetMark2 SetMark3 SetMark4 SetMark5 SetMark6 SetMark7 SetMark8 SetMark9 ShowFileFilter ShowInfoPixel ShowMarks SlideShow SlideShowFaster SlideShowPause SlideShowSlower SplitDownPane SplitHorizontal SplitNextPane SplitPaneSync SplitPreviousPane SplitQuad SplitSingle SplitTriple SplitUpPane SplitVertical StereoAuto StereoCross StereoCycle StereoOff StereoSBS Thumbnails ToggleMark0 ToggleMark1 ToggleMark2 ToggleMark3 ToggleMark4 ToggleMark5 ToggleMark6 ToggleMark7 ToggleMark8 ToggleMark9 UnselMark0 UnselMark1 UnselMark2 UnselMark3 UnselMark4 UnselMark5 UnselMark6 UnselMark7 UnselMark8 UnselMark9 Up UseColorProfiles UseImageProfile ViewIcons ViewInNewWindow ViewList WriteRotation WriteRotationKeepDate Zoom100 Zoom200 Zoom25 Zoom300 Zoom33 Zoom400 Zoom50 ZoomFillHor ZoomFillVert ZoomFit ZoomIn ZoomOut ZoomToRectangle'
+actions='About AddMark0 AddMark1 AddMark2 AddMark3 AddMark4 AddMark5 AddMark6 AddMark7 AddMark8 AddMark9 AlterNone Animate Back ClearMarks CloseWindow ColorProfile0 ColorProfile1 ColorProfile2 ColorProfile3 ColorProfile4 ColorProfile5 ConnectZoom100 ConnectZoom200 ConnectZoom25 ConnectZoom300 ConnectZoom33 ConnectZoom400 ConnectZoom50 ConnectZoomFillHor ConnectZoomFillVert ConnectZoomFit ConnectZoomIn ConnectZoomOut Copy CopyPath CopyPathUnquoted CropFourThree CropNone CropOneOne CropRectangle CropSixteenNine CropThreeTwo CutPath Delete DeleteWindow DrawRectangle Escape ExifRotate ExifWin FilterMark0 FilterMark1 FilterMark2 FilterMark3 FilterMark4 FilterMark5 FilterMark6 FilterMark7 FilterMark8 FilterMark9 FindDupes FirstImage FirstPage Flip FloatTools FolderTree Forward FullScreen Grayscale HelpChangeLog HelpContents HelpKbd HelpNotes HelpSearch HelpShortcuts HideBars HideSelectableToolbars HideTools HistogramChanB HistogramChanCycle HistogramChanG HistogramChanR HistogramChanRGB HistogramChanV HistogramModeCycle HistogramModeLin HistogramModeLog Home IgnoreAlpha ImageBack ImageForward ImageHistogram ImageOverlay ImageOverlayCycle IntMark0 IntMark1 IntMark2 IntMark3 IntMark4 IntMark5 IntMark6 IntMark7 IntMark8 IntMark9 KeywordAutocomplete LastImage LastPage LayoutConfig LogWindow Maintenance Mark0 Mark1 Mark2 Mark3 Mark4 Mark5 Mark6 Mark7 Mark8 Mark9 Mirror Move NewCollection NewFolder NewWindow NewWindowDefault NewWindowFromCurrent NextImage NextPage OpenArchive OpenCollection OpenRecent OpenWith OverUnderExposed PanView PermanentDelete Plugins Preferences PrevImage PrevPage Print Quit Rating0 Rating1 Rating2 Rating3 Rating4 Rating5 RatingM1 RectangularSelection Refresh Rename RenameWindow ResetMark0 ResetMark1 ResetMark2 ResetMark3 ResetMark4 ResetMark5 ResetMark6 ResetMark7 ResetMark8 ResetMark9 Rotate180 RotateCCW RotateCW SBar SBarSort SaveMetadata Search SearchAndRunCommand SelectAll SelectInvert SelectMark0 SelectMark1 SelectMark2 SelectMark3 SelectMark4 SelectMark5 SelectMark6 SelectMark7 SelectMark8 SelectMark9 SelectNone SetMark0 SetMark1 SetMark2 SetMark3 SetMark4 SetMark5 SetMark6 SetMark7 SetMark8 SetMark9 ShowFileFilter ShowInfoPixel ShowMarks SlideShow SlideShowFaster SlideShowPause SlideShowSlower SplitDownPane SplitHorizontal SplitNextPane SplitPaneSync SplitPreviousPane SplitQuad SplitSingle SplitTriple SplitUpPane SplitVertical StereoAuto StereoCross StereoCycle StereoOff StereoSBS Thumbnails ToggleMark0 ToggleMark1 ToggleMark2 ToggleMark3 ToggleMark4 ToggleMark5 ToggleMark6 ToggleMark7 ToggleMark8 ToggleMark9 UnselMark0 UnselMark1 UnselMark2 UnselMark3 UnselMark4 UnselMark5 UnselMark6 UnselMark7 UnselMark8 UnselMark9 Up UseColorProfiles UseImageProfile ViewIcons ViewInNewWindow ViewList WriteRotation WriteRotationKeepDate Zoom100 Zoom200 Zoom25 Zoom300 Zoom33 Zoom400 Zoom50 ZoomFillHor ZoomFillVert ZoomFit ZoomIn ZoomOut ZoomToRectangle'
 
 options_basic='--blank --cache-maintenance= --disable-clutter --fullscreen --geometry= --help --list --new-instance --log-file= --remote --slideshow --with-tools --without-tools --version --show-log-window --debug= --grep='
 
-options_remote='--action= --action-list --back --close-window --config-load= --cache-metadata --cache-render= --cache-render-recurse= --cache-render-shared= --cache-render-shared-recurse= --cache-shared= --cache-thumbs= --delay= --first --fullscreen --file= --File= --fullscreen-start --fullscreen-stop --geometry= --get-collection= --get-collection-list --get-destination= --get-file-info --get-filelist= --get-filelist-recurse= --get-rectangle --get-render-intent --get-selection --get-sidecars= --get-window-list --id= --last --list-add= --list-clear --lua= --new-window --next --pixel-info --print0 --PWD= --quit --raise --selection-add= --selection-clear --selection-remove= --slideshow --slideshow-recurse= --slideshow-start --slideshow-stop --tell --tools-hide --view='
+options_remote='--action= --action-list --back --close-window --config-load= --cache-metadata --cache-render= --cache-render-recurse= --cache-render-shared= --cache-render-shared-recurse= --cache-shared= --cache-thumbs= --delay= --first --fullscreen --file= --File= --fullscreen-start --fullscreen-stop --geometry= --get-collection= --get-collection-list --get-destination= --get-file-info --get-filelist= --get-filelist-recurse= --get-rectangle --get-render-intent --get-selection --get-sidecars= --get-window-list --id= --last --list-add= --list-clear --lua= --new-window --next --pixel-info --print0 --PWD= --quit --raise --selection-add= --selection-clear --selection-remove= --slideshow --slideshow-recurse= --slideshow-start --slideshow-stop --tell --tools-hide --tools-show --view='
 
 _geeqie()
 {

scripts/test-ancillary-files.sh
@@ -22,7 +22,7 @@
 ## @file
 ## @brief Perform validity checks on project ancillary files
 ##
-## $1 Root of project sources
+## $1 Root of project sources or NULL for current
 ##
 ## Perform validity checks on project ancillary files:
 ## appdata
@@ -32,7 +32,10 @@
 ## xml
 ##
 
-cd "$1" || exit 1
+if [ -n "$1" ]
+then
+	cd "$1" || exit 1
+fi
 
 if [ ! -d "src" ] || [ ! -f "geeqie.1" ]
 then
@@ -57,7 +60,7 @@ do
 		fi
 	fi
 done << EOF
-$(find "$1/plugins" "$1/src" "$1/scripts" -type f -not -name downsize -executable)
+$(find "./plugins" "./src" "./scripts" -type f -not -name downsize -executable)
 EOF
 
 # Script files must have the file extension .sh  or
@@ -81,7 +84,7 @@ do
 		fi
 	fi
 done << EOF
-$(find "$1/plugins" "$1/src" "$1/scripts" -type f -executable)
+$(find "./plugins" "./src" "./scripts" -type f -executable)
 EOF
 
 # Check if all options are in the disabled checks
@@ -89,7 +92,7 @@ while read -r line
 do
 	if [ -n "$line" ]
 	then
-		res=$(grep "$line" "$1/scripts/test-all.sh")
+		res=$(grep "$line" "./scripts/test-all.sh")
 		if [ -z "$res" ]
 		then
 			printf "ERROR; Option no disabled check in ./scripts/test-all.sh: %s\n" "$line"
@@ -97,7 +100,7 @@ do
 		fi
 	fi
 done << EOF
-$(awk 'BEGIN {FS="\047"} /option/ { if (substr($2,0,2) != "gq") { print $2 } }' meson_options.txt)
+$(awk --lint=fatal --posix 'BEGIN {FS="\047"} /option\(/ { if (substr($2, 1, 2) != "gq") { print $2 } }' meson_options.txt)
 EOF
 
 # Check if all options are in the disabled checks in a GitHub run
@@ -108,7 +111,7 @@ then
 	do
 		if [ -n "$line" ]
 		then
-			res=$(grep "\-D$line=disabled" "$1/.github/workflows/check-build-actions.yml")
+			res=$(grep "\-D$line=disabled" "./.github/workflows/check-build-actions.yml")
 			if [ -z "$res" ]
 			then
 				printf "ERROR; Option no disabled check in .github/workflows/check-build-actions.yml: %s\n" "$line"
@@ -116,7 +119,7 @@ then
 			fi
 		fi
 	done << EOF
-$(awk 'BEGIN {FS="\047"} /option/ { if (substr($2,0,2) != "gq") { print $2 } }' meson_options.txt)
+$(awk --lint=fatal --posix 'BEGIN {FS="\047"} /option\(/ { if (substr($2, 1, 2) != "gq") { print $2 } }' meson_options.txt)
 EOF
 fi
 
@@ -217,10 +220,10 @@ else
 		if [ -n "$line" ]
 		then
 			desktop_file=$(basename "$line" ".in")
-			ln --symbolic "$line" "$1/$desktop_file"
-			result=$(desktop-file-validate "$1/$desktop_file")
+			ln --symbolic "$line" "./$desktop_file"
+			result=$(desktop-file-validate "./$desktop_file")
 
-			rm "$1/$desktop_file"
+			rm "./$desktop_file"
 			if [ -n "$result" ]
 			then
 				printf "ERROR; desktop-file-validate error in: %s %s\n" "$line" "$result"
@@ -276,5 +279,238 @@ else
 $(find ./doc/docbook -name "*.xml")
 EOF
 fi
+
+# Command line completion
+## Check the sections: actions, options_basic, options_remote
+## The file_types section is not checked.
+## Look for options not included and options erroneously included.
+
+if [ ! -d ./build ]
+then
+	meson setup build
+	ninja -C build
+else
+	if [ ! -f ./build/src/geeqie ]
+	then
+		ninja -C build
+	fi
+fi
+
+geeqie_exe=$(realpath ./build/src/geeqie)
+
+actions_cc=$(mktemp "${TMPDIR:-/tmp}/geeqie.XXXXXXXXXX")
+actions_help=$(mktemp "${TMPDIR:-/tmp}/geeqie.XXXXXXXXXX")
+actions_help_cut=$(mktemp "${TMPDIR:-/tmp}/geeqie.XXXXXXXXXX")
+help_output=$(mktemp "${TMPDIR:-/tmp}/geeqie.XXXXXXXXXX")
+options_basic_cc=$(mktemp "${TMPDIR:-/tmp}/geeqie.XXXXXXXXXX")
+options_basic_help=$(mktemp "${TMPDIR:-/tmp}/geeqie.XXXXXXXXXX")
+options_remote_cc=$(mktemp "${TMPDIR:-/tmp}/geeqie.XXXXXXXXXX")
+options_remote_help=$(mktemp "${TMPDIR:-/tmp}/geeqie.XXXXXXXXXX")
+
+options_basic1=$(grep 'options_basic=' ./auto-complete/geeqie)
+options_basic2=$(echo "$options_basic1" | cut -c 16-)
+options_basic3=$(echo "$options_basic2" | sed "s/\x27//g")
+options_basic4=$(echo "$options_basic3" | sed "s/ /\n/g")
+echo "$options_basic4" | sort > "$options_basic_cc"
+
+options_remote1=$(grep 'options_remote=' ./auto-complete/geeqie)
+options_remote2=$(echo "$options_remote1" | cut -c 17-)
+options_remote3=$(echo "$options_remote2" | sed "s/\x27//g")
+options_remote4=$(echo "$options_remote3" | sed "s/ /\n/g")
+echo "$options_remote4" | sort > "$options_remote_cc"
+
+action_list1=$(grep 'actions=' ./auto-complete/geeqie)
+action_list2=$(echo "$action_list1" | cut --delimiter='=' --fields=2)
+action_list3=$(echo "$action_list2" | sed "s/\x27//g")
+action_list4=$(echo "$action_list3" | sed 's/ /\n/g')
+echo "$action_list4" | sort > "$actions_cc"
+
+./scripts/isolate-test.sh xvfb-run --auto-servernum "$geeqie_exe" --help > "$help_output"
+
+awk --lint=fatal --posix --assign options_basic_help="$options_basic_help" --assign options_remote_help="$options_remote_help" '
+BEGIN {
+valid_found = 0
+}
+
+/Valid options/ {valid_found = 1}
+/Remote command/ {valid_found = 0}
+/--/ && valid_found {
+	start = match($0, /--/)
+	new=substr($0, start)
+	{gsub(/ .*/, "", new)}
+	{gsub(/=.*/, "=", new)}
+	{gsub(/\[.*/, "", new)}
+	print new >> options_basic_help
+	}
+
+/--/ && ! valid_found {
+	start = match($0, /--/)
+	new = substr($0, start)
+	{gsub(/ .*/, "", new)}
+	{gsub(/=.*/, "=", new)}
+	{gsub(/\[.*/, "", new)}
+	print new >> options_remote_help
+	}
+
+END {
+close(options_basic_help)
+close(options_remote_help)
+}
+' "$help_output"
+
+# https://backreference.org/2010/02/10/idiomatic-awk/ is a good reference
+awk --lint=fatal --posix '
+BEGIN {
+exit_status = 0
+}
+
+NR == FNR{a[$0]="";next} !($0 in a) {
+	exit_status = 1
+	print "Bash completions - Basic option missing: " $0
+	}
+
+END {
+exit exit_status
+}
+' "$options_basic_cc" "$options_basic_help"
+
+if [ $? = 1 ]
+then
+	exit_status=1
+fi
+
+awk --lint=fatal --posix '
+BEGIN {
+exit_status = 0
+}
+
+NR == FNR{a[$0]="";next} !($0 in a) {
+	exit_status = 1
+	print "Bash completions - Basic option error: " $0
+	}
+
+END {
+exit exit_status
+}
+' "$options_basic_help" "$options_basic_cc"
+
+if [ $? = 1 ]
+then
+	exit_status=1
+fi
+
+awk --lint=fatal --posix '
+BEGIN {
+exit_status = 0
+}
+
+NR == FNR{a[$0]="";next} !($0 in a) {
+	exit_status = 1
+	print "Bash completions - Remote option missing: " $0
+	}
+
+END {
+exit exit_status
+}
+' "$options_remote_cc" "$options_remote_help"
+
+if [ $? = 1 ]
+then
+	exit_status=1
+fi
+
+## @FIXME Differing configuration options are not handled
+awk --lint=fatal --posix '
+BEGIN {
+exit_status = 0
+}
+
+NR == FNR{a[$0]="";next} !($0 in a) {
+	if (index($0, "lua") == 0)
+		{
+		exit_status = 1
+		print "Bash completions - Remote option error: " $0
+		}
+	}
+
+END {
+exit exit_status
+}
+' "$options_remote_help" "$options_remote_cc"
+
+if [ $? = 1 ]
+then
+	exit_status=1
+fi
+
+./scripts/isolate-test.sh xvfb-run --auto-servernum "$geeqie_exe" --remote --action-list --quit | cut --delimiter=' ' --fields=1 | sed '/^$/d' > "$actions_help"
+
+awk --lint=fatal --posix --assign actions_help_cut="$actions_help_cut" '
+BEGIN {
+remote_found = 0
+}
+
+/Remote/ {
+	remote_found = 1
+	next
+	}
+
+/.*?/ && remote_found {
+	print $0 >> actions_help_cut
+	}
+
+END {
+close(actions_help_cut)
+}
+' "$actions_help"
+
+awk --lint=fatal --posix '
+BEGIN {
+exit_status = 0
+}
+
+NR == FNR{a[$0]="";next} !($0 in a) {
+	print "Bash completions - Action missing: " $0
+	exit_status = 1
+	}
+
+END {
+exit exit_status
+}
+' "$actions_cc" "$actions_help_cut"
+
+if [ $? = 1 ]
+then
+	exit_status=1
+fi
+
+awk --lint=fatal --posix '
+BEGIN {
+exit_status = 0
+}
+
+NR == FNR{a[$0]="";next} !($0 in a) {
+	print "Bash completions - Action error: " $0
+	exit_status = 1
+	}
+
+END {
+exit exit_status
+}
+' "$actions_help_cut" "$actions_cc"
+
+if [ $? = 1 ]
+then
+	exit_status=1
+fi
+
+rm --force "$actions_cc"
+rm --force "actions_help"
+rm --force "actions_help_cut"
+rm --force "help_output"
+rm --force "options_basic_cc"
+rm --force "options_basic_help"
+rm --force "options_remote_cc"
+rm --force "options_remote_help"
 
 exit "$exit_status"

src/main.cc
@@ -498,7 +498,7 @@ static void parse_command_line(gint argc, gchar *argv[])
 				print_term(FALSE, _("  -v, --version                    print version info\n"));
 				print_term(FALSE, _("  -w, --show-log-window            show log window\n"));
 #ifdef DEBUG
-				print_term(FALSE, _("      --debug[=level]              turn on debug output\n"));
+				print_term(FALSE, _("      --debug=[level]              turn on debug output\n"));
 				print_term(FALSE, _("  -g, --grep=<regexp>              filter debug output\n"));
 #endif
 

src/ui/menu-classic.ui
@@ -97,11 +97,11 @@
         <separator/>
         <menuitem action="DrawRectangle"/>
         <menu action="AspectRatioMenu">
-          <menuitem action="None"/>
-          <menuitem action="OneOne"/>
-          <menuitem action="FourThree"/>
-          <menuitem action="ThreeTwo"/>
-          <menuitem action="SixteenNine"/>
+          <menuitem action="CropNone"/>
+          <menuitem action="CropOneOne"/>
+          <menuitem action="CropFourThree"/>
+          <menuitem action="CropThreeTwo"/>
+          <menuitem action="CropSixteenNine"/>
         </menu>
         <menuitem action="CropRectangle"/>
         <separator/>

src/ui/menu-hamburger.ui
@@ -98,11 +98,11 @@
         <separator/>
         <menuitem action="DrawRectangle"/>
         <menu action="AspectRatioMenu">
-          <menuitem action="None"/>
-          <menuitem action="OneOne"/>
-          <menuitem action="FourThree"/>
-          <menuitem action="ThreeTwo"/>
-          <menuitem action="SixteenNine"/>
+          <menuitem action="CropNone"/>
+          <menuitem action="CropOneOne"/>
+          <menuitem action="CropFourThree"/>
+          <menuitem action="CropThreeTwo"/>
+          <menuitem action="CropSixteenNine"/>
         </menu>
         <menuitem action="CropRectangle"/>
         <separator/>

commit fb7cbc54ee99a79edadd5e8aaae302a8597539a4

Bug fix: Correct run-time warning
Crop action renamed.
Bug introduced in commit 138b03ff

src/layout-util.cc
@@ -3866,7 +3866,7 @@ static void layout_util_sync_views(LayoutWindow *lw)
 	action = gq_gtk_action_group_get_action(lw->action_group, "ViewIcons");
 	gq_gtk_radio_action_set_current_value(GTK_RADIO_ACTION(action), lw->options.file_view_type);
 
-	action = gq_gtk_action_group_get_action(lw->action_group, "None");
+	action = gq_gtk_action_group_get_action(lw->action_group, "CropNone");
 	gq_gtk_radio_action_set_current_value(GTK_RADIO_ACTION(action), options->rectangle_draw_aspect_ratio);
 
 	action = gq_gtk_action_group_get_action(lw->action_group, "FloatTools");