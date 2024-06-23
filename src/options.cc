4f8a49858a4e84fd64d81e7a99143c1ca71718c2
Colin Clark <colin.clark@cclark.uk>
2024-06-23 18:10:41 +0100

    Change default value of Remember Dialog Window Positions
    Change default value from "Not Saved" to "Saved"

--- src/options.cc
+++ src/options.cc
@@ -79,7 +79,7 @@ ConfOptions *init_options(ConfOptions *options)
 	options->use_saved_window_positions_for_new_windows = FALSE;
 	options->save_window_workspace = FALSE;
 	options->tools_restore_state = TRUE;
-	options->save_dialog_window_positions = FALSE;
+	options->save_dialog_window_positions = TRUE;
 	options->hide_window_decorations = FALSE;
 	options->show_window_ids = FALSE;
 
