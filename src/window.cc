00529df8677c2a4bdc58944dcd73849bbee9fb02
Colin Clark <colin.clark@cclark.uk>
2024-09-08 14:49:35 +0100

    Include the Help file in pdf format (2)
    At the moment the help.pdf file is generated outside the Geeqie project
    and uploaded to geeqie.org along with the other help files.
    
    This is done on https://github.com/geeqie/geeqie.github.io
    
    The Help menu code looks for the local file in gq_helpdir, does not find
    it and defaults to the copy on geeqie.org.
    
    This will be corrected when the help.pdf file can be generated from the
    Docbook xml files.

--- src/window.cc
+++ src/window.cc
@@ -418,4 +418,29 @@ void help_search_window_show()
 
 	gtk_widget_show(gd->dialog);
 }
+
+void help_pdf()
+{
+	GError *error = nullptr;
+	gchar *path;
+	gchar *command;
+
+	path = g_build_filename(gq_helpdir, "help.pdf", NULL);
+
+	if (!isfile(path))
+		{
+		path = g_build_filename("https://www.geeqie.org/help-pdf/help.pdf", NULL);
+		}
+
+	command = g_strdup_printf("xdg-open %s", path);
+
+	if (!g_spawn_command_line_async(command, &error))
+		{
+		log_printf(_("Warning: Failed to execute command: %s\n"), error->message);
+		g_error_free(error);
+		}
+
+	g_free(path);
+	g_free(command);
+}
 /* vim: set shiftwidth=8 softtabstop=0 cindent cinoptions={1s: */
