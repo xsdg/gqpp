0b20b0623e3c003d382e6ddd2bc8bbb446c51ca2
Colin Clark <colin.clark@cclark.uk>
2025-01-10 16:39:20 +0000

    Fix problems with extracted AppImage
    Problems reported with linuxdeploy AppImages are:
    https://github.com/linuxdeploy/linuxdeploy/issues/303
    https://github.com/linuxdeploy/linuxdeploy/issues/304
    
    This patch is a temporary workaround.

--- scripts/geeqie-download-appimage.sh
+++ scripts/geeqie-download-appimage.sh
@@ -330,4 +330,13 @@ then
 	else
 		ln --symbolic --force "Geeqie$minimal-latest-$architecture.AppImage" geeqie
 	fi
+
+	# Fix problems in linuxdeploy
+	# https://github.com/linuxdeploy/linuxdeploy/issues/303
+	# https://github.com/linuxdeploy/linuxdeploy/issues/304
+	# shellcheck disable=SC2016
+	sed -i '/^this_dir/ c\this_dir=$(dirname "$(readlink -f "$BASH_SOURCE")")' "./Geeqie$minimal-latest-$architecture-AppImage/squashfs-root/AppRun"
+
+	# shellcheck disable=SC2016
+	sed -i '/^exec/ c\exec $(readlink -f $this_dir/AppRun.wrapped) "$@"' "./Geeqie$minimal-latest-$architecture-AppImage/squashfs-root/AppRun"
 fi
